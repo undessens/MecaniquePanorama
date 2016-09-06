ae10f458f3aadf26a3e1342b719c8f912073bee1
CocoTosh <admin@Mac-Pro-de-admin.local>
2016-09-06 12:04:30 +0200

    python starting automatically of app

--- compte-tour.py
+++ compte-tour.py
@@ -1,14 +1,36 @@
 import serial
 import time
+import os
 from OSC import OSCClient, OSCMessage
 
+#OS choose ofApp working directory
+os.chdir('ImageSeqFromOSC/')
 
+def lauch_app():
+	
+	os.system('make run')
 
+def stop_app():
+	os.system('killall "ImageSeqFromOSC" ')
 
+#Serial
+try:
+	ser = serial.Serial('/dev/ttyACM0',115200)
+	#ser = serial.Serial('/dev/ttyACM1', 115200)
+except :
+	print "ValueError"
+	ser = None	
+
+#Start OF app
+stop_app()
+time.sleep(1)
+lauch_app(	)
+
+#OSC
 client = OSCClient()
 client.connect( ("localhost", 12345))
-#ser = serial.Serial('/dev/ttyACM1', 115200)
-ser = serial.Serial('/dev/ttyACM0',115200)
+
+
 value = 0
 param = []
 msg = []
@@ -19,8 +41,20 @@ time.sleep(1)
 
 
 while 1 :
-	msg = (ser.readline())
+	
+	if(ser):
+		msg = (ser.readline())
+	else :	
+		msg = ""
+
+
 	if (len(msg)>0):
+	##########
+	#TODO
+	# ajouter un numero en plus au msg, lié a la vitesse
+	#rajouter cette vitesse dans le message osc
+	# changer les images en fonction de cette vitesse
+	#########
 		
 		
 
@@ -38,3 +72,5 @@ while 1 :
 			print e
 		
 	time.sleep(0.01)
+
+
